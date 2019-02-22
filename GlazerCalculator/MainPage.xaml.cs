0a99a951b64c11b725565fa4c8c89f3f0d6d74bd
samanthahancock <han13018@byui.edu>
2019-02-22 11:40:57 -0700

    Still working on the small details

--- GlazerCalculator/MainPage.xaml.cs
+++ GlazerCalculator/MainPage.xaml.cs
@@ -25,6 +25,70 @@ namespace GlazerCalculator
         public MainPage()
         {
             this.InitializeComponent();
+
+        }
+
+        private void ButCalculate_Click(object sender, RoutedEventArgs e)
+        {
+
+            double width, height, woodLength, glassArea;
+            width = double.Parse(txtboxWidthInput.Text);
+            height = double.Parse(txtboxHeightInput.Text);
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height) * 3.25;
+            double quant = double.Parse(quantityNum.Text);
+            string tint = ((ComboBoxItem)tintDropDown.SelectedItem).Content.ToString(); ;
+            output.Text = $"Quantity: {quant}\n" +
+                $"Tint: {tint}\n" +
+                $"Length: {woodLength}\n" +
+                $"Area: {glassArea}";
+            //txtDate.Text = DateTime.Now.ToString();
+
+        }
+
+        private void TxtboxWidthInput_KeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            string widthString = txtboxWidthInput.Text;
+            if (!int.TryParse(widthString, out int width))
+            {
+                txtboxWidthInput.BorderBrush = new SolidColorBrush(Windows.UI.Colors.Red);
+                txtboxWidthInput.Foreground = new SolidColorBrush(Windows.UI.Colors.Red);
+                widthWarning.Text = "Width must be a number.";
+            }
+            else
+            {
+                txtboxWidthInput.BorderBrush = new SolidColorBrush(Windows.UI.Colors.Gray);
+                widthWarning.Text = "";
+            }
+        }
+
+        private void TxtboxHeightInput_KeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            string heightString = txtboxHeightInput.Text;
+            if (!int.TryParse(heightString, out int width))
+            {
+                txtboxHeightInput.BorderBrush = new SolidColorBrush(Windows.UI.Colors.Red);
+                txtboxHeightInput.Foreground = new SolidColorBrush(Windows.UI.Colors.Red);
+                heightWarning.Text = "Height must be a number.";
+            }
+            else
+            {
+                txtboxHeightInput.BorderBrush = new SolidColorBrush(Windows.UI.Colors.Gray);
+                heightWarning.Text = "";
+            }
+        }
+
+            private void SliderQuantity_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            quantityNum.Text = sliderQuantity.Value.ToString();
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
+
 }
+    
